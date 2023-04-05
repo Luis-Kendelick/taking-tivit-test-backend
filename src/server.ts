eb23ad0433fa9613139fd014edf73eb6b63abb10
Luis Fritzen <luisfafritzen@gmail.com>
2023-04-04 23:32:48 -0300

    feat: create test server

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express'
+import dotenv from 'dotenv'
+import http from 'http'
 
-dotenv.config();
-const app = express();
+dotenv.config()
+const app = express()
 
-const serverPort = process.env.PORT || 3333;
+const server = http.createServer()
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World!" });
-});
+const serverPort = process.env.PORT ?? 3333
 
-app.listen(serverPort, () => {
-  console.log("Server started on port 3333!!");
-});
+app.get('/', (request, response) => response.json({ message: 'Hello World!' }))
+
+server.listen(serverPort, () => {
+  console.log('Server started on port 3333!!')
+})
