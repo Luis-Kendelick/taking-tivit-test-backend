2ce067f66e530eb6eee00b6f2cb50c227fb2c8ca
Luis Fritzen <luisfafritzen@gmail.com>
2023-04-11 01:38:38 -0300

    feat: init app with mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import express from 'express'
+import dotenv from 'dotenv'
+import userRouter from './routes/authRoutes'
+import cors from 'cors'
+
+dotenv.config()
 
 const app = express()
+const serverPort = process.env.PORT ?? 3333
 
-app.use((request, response, next) => {
-  response.status(200).json({
-    message: 'It works!'
-  })
-})
+app.use(express.json())
+
+app.use(cors())
+
+app.listen(serverPort)
+
+app.use('/users', userRouter)
+
+export default app
